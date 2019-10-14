9d072045f99845ebac8487e68686d299a3d07c6a
Liudmyla Husar <liudmyla.husar@gmail.com>
2019-10-14 19:35:34 +0300

    Add pagination into block Add SASS

--- src/components/blocks/blocks.js
+++ src/components/blocks/blocks.js
@@ -3,13 +3,40 @@ import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
 import { connect } from 'react-redux';
 import Table from '../table/table';
+import Pagination from 'react-js-pagination';
+import './blocks.scss';
+import { PREVIOUS, NEXT } from '../../constants';
 
 class Blocks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+  
+  handlePageChange(pageNumber) {
+    this.setState({activePage: pageNumber});
+  }
+  
   render() {
     const { blockList = [] }  = this.props;
     return (
       <Container>
-        <h3 className="headers">Blocks</h3>
+        <Pagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={10}
+          totalItemsCount={blockList.length}
+          pageRangeDisplayed={1}
+          onChange={this.handlePageChange.bind(this)}
+          prevPageText={ PREVIOUS }
+          nextPageText={ NEXT }
+          innerClass={'pagination'}
+          activeClass={'pagination-element'}
+          activeLinkClass={'pagination-text'}
+          linkClass={'pagination-link'}
+          disabledClass={'pagination-disabled'}
+        />
         <Table blockList={blockList}/>
       </Container>
     );}
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,6 @@
 export const apiUrl = 'https://blockchain.info/blocks/';
+
+//blocks
+export const PREVIOUS = 'Previous';
+export const NEXT = 'Next';
+
